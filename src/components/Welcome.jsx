55bd07d0526b81a3b5ac8ef631a9bc8364940060
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2023-07-20 15:38:10 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -37,7 +37,7 @@ const Welcome = () =>{
     return(
         
          <div className="flex w-full justify-center items-center">
-            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-10 ml-10">
                 <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white py-1">
                         Exchange Crypto <br /> Across The World
